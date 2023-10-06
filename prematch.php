601a4426d83f38ee1ba80d12fe55429ecd0a06d4
repcak1 <113353486+repcak1@users.noreply.github.com>
2023-10-06 12:30:29 +0200

    Update prematch.php

--- prematch.php
+++ prematch.php
@@ -7,3 +7,14 @@ else
 echo "nie znalezniono wzorca";
 echo "znaleziono: ". preg_match_all($wzorzec,$tekst);
  ?>
+
+
+ <?php
+$wzorzec = '/\d/';
+$tekst = "mam 4 zeszyty ";
+if( preg_match( $wzorzec,$tekst) )
+echo "znaleziono cyfre";
+else 
+echo "nie znalezniono cyfre";
+echo "znaleziono: ". preg_match_all($wzorzec,$tekst);
+ ?>
